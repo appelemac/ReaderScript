a7575cb9a42fb7c25546cd220a4b5deeb536f595
appelemac <appelemac@gmail.com>
2016-05-28 16:23:28 +0200

    base

--- base.user.js
+++ base.user.js
@@ -3,8 +3,8 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  Load every Chapter
-// @author       You
-// @match        http://gravitytales.com/zhan-long/
+// @author       Appelemac
+// @include      http://gravitytales.com/*
 // @grant        none
 // ==/UserScript==
 
