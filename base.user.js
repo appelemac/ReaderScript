94d6e6323c1b8b848fc9aae510e64e6aacf9d7a2
appelemac <appelemac@gmail.com>
2016-05-28 16:13:27 +0200

    base

---
+++ base.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Reader
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Load every Chapter
+// @author       You
+// @match        http://gravitytales.com/zhan-long/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var elements = document.getElementsByClassName("entry-title"); 
+    for (var i = 0; i <elements.length; i++) {
+    var el = elements[i];
+        el.InnerHTML = "cool";
+    }
+    
+})();
